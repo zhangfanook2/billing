83fdd4827ea8df0ed1c050289129f590baef1ef0
zhangfanook2 <zhangfanook@sina.com>
2025-10-28 10:26:54 +0800

    welcome22

--- src/main/java/com/akamai/Main.java
+++ src/main/java/com/akamai/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.print("Hello and welcome22!");
+        System.out.print("Hello and welcome222!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
